4bd635b0ebf463d8a273f4a3ae35f825a3b6f8b1
GMw3 <w3ghulam@gmail.com>
2022-12-22 12:03:20 +0500

    Make Entering Screen Responsive

--- src/components/enteringSection/index.jsx
+++ src/components/enteringSection/index.jsx
@@ -9,19 +9,19 @@ const EnteringSection = ({setScreen, screenValue}) => {
   }
   return (
     <>
-      <header className="relative flex items-center justify-center h-screen  overflow-hidden">
+      <div className="relative flex items-center justify-center h-screen overflow-hidden">
         <video
           autoPlay
           loop
           muted
-          className="absolute z-10 w-auto min-w-full min-h-full max-w-none">
+          className="absolute w-auto min-w-full min-h-full">
           <source src={videos} type="video/mp4" />
         </video>
-      </header>
-      <div onClick={EnterSection} className="absolute top-[1050px] left-[1050px] flex justify-center items-center z-20">
+      <div onClick={EnterSection} className="relative top-[40%] flex justify-center">
         <button className="outline outline-2 m-4 p-3 px-14 text-white bg-transparent text-2xl hover:bg-white hover:text-red-900">
           ENTER
         </button>
+      </div>
       </div>
     </>
   );
