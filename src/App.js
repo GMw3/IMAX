96a262778d6f48e19bbea4e166a5aca8c6dfdd23
Ghulam Murtaza <w3ghulam@gmail.com>
2023-02-08 23:00:11 +0500

    Added mint Route, Mint Component, Smart Contract Integeration.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import Aos from "aos";
 import "aos/dist/aos.css";
 import React, { useEffect, useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { toast } from "react-toastify";
+import { ethers } from "ethers";
 
+import { ContractABI } from "./components/Mint/Contract";
 const background = require("./images/Background/bg.svg").default;
 import CoreTeam from "./components/core_team/CoreTeam";
 import EnteringSection from "./components/enteringSection";
@@ -14,14 +20,109 @@ import NFT from "./components/NFT";
 import Roadmap from "./components/roadmap/Roadmap";
 import SharedModal from "./components/SharedModal/SharedModal";
 import Team from "./components/team/Team";
-
+import Mint from "./components/Mint/Mint.jsx";
 import "./App.css";
 
 function App() {
   const [showVidModal, setShowVidModal] = useState(false);
   const [video_src, setVideo_src] = useState("");
   const [enter, setEnter] = useState(false);
+  const [wallet, setWallet] = useState("Connect a Wallet");
+  const [logout, setLogout] = useState(false);
+  const [maxMintAmount, setMaxMintAmount] = useState();
+  const [price, setPrice] = useState(0);
+  const [images, setImages] = useState([]);
+  const [userMintedAmount, setUserMintedAmount] = useState(0);
 
+  const { REACT_APP_NETWORK } = process.env;
+  const { REACT_APP_NETWORK_CHAIN_ID } = process.env;
+  const { REACT_APP_CONTRACT_ADDRESS } = process.env;
+
+  const notify = (message) => {
+    toast.error(message, {
+      toastId: "custom-id-yes",
+    });
+  };
+  const setupConnections = async () => {
+    if (window.ethereum != null) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const network = await provider.getNetwork();
+      if (REACT_APP_NETWORK !== network.name) {
+        notify(
+          `Not on a correct network. Change your network to "${REACT_APP_NETWORK}"`
+        );
+        return false;
+      } else {
+        await provider.send("eth_requestAccounts", []);
+        const signer = await provider.getSigner();
+        const address = await signer.getAddress();
+        return address;
+      }
+    } else {
+      notify("No Ether wallet available");
+      return false;
+    }
+  };
+  const connection = async () => {
+    const res = await setupConnections();
+    if (res === false) {
+      setWallet("Connect a Wallet");
+      await window.ethereum.request({
+        method: "wallet_switchEthereumChain",
+        params: [{ chainId: REACT_APP_NETWORK_CHAIN_ID }],
+      });
+    } else {
+      setLogout(true);
+      setWallet(res.slice(0, 6) + "..." + res.slice(36, 42));
+    }
+  };
+  const disconnect = async () => {
+    setWallet("Connect a Wallet");
+    setLogout(false);
+    setUserMintedAmount(0);
+    setMaxMintAmount("-");
+    setPrice("-");
+    setImages([]);
+  };
+  const readContract = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(
+      REACT_APP_CONTRACT_ADDRESS,
+      ContractABI,
+      provider
+    );
+    const maxMintAmount = await contract.maxMintAmount();
+    let accounts = await provider.send("eth_requestAccounts", []);
+    let address = accounts[0];
+    const userMintedAmount = await contract.balanceOf(address);
+    const price = await contract.cost();
+    setMaxMintAmount(parseInt(maxMintAmount, 10));
+    setUserMintedAmount(parseInt(userMintedAmount, 10));
+    setPrice(Number(ethers.utils.formatEther(price)));
+  };
+  const getTokens = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+    const contract = new ethers.Contract(
+      REACT_APP_CONTRACT_ADDRESS,
+      ContractABI,
+      provider
+    );
+    let accounts = await provider.send("eth_requestAccounts", []);
+    let address = accounts[0];
+    const imagesTockens = await contract.nftsOnwedByWallet(address);
+    let imagesLocal = [];
+    await imagesTockens.map(async (image) => {
+      const url = await contract.tokenURI(parseInt(image, 10));
+      let result = await url.replace("ipfs://", "https://ipfs.io/ipfs/");
+      const jsonBody = await (await fetch(result)).json();
+      imagesLocal.push(
+        await jsonBody.image.replace("ipfs://", "https://ipfs.io/ipfs/")
+      );
+    });
+    setTimeout(() => {
+      setImages(imagesLocal);
+    }, [3000]);
+  };
   const enable_Modal = (vid_src) => {
     setVideo_src(vid_src);
     setShowVidModal(true);
@@ -35,15 +136,70 @@ function App() {
     <>
       {enter ? (
         <div className="bg-background-img">
-          <Navbar />
-          <LandingSection />
-          <Lore />
-          <NFT />
-          <Roadmap enable_Modal={enable_Modal} />
-          <CoreTeam />
-          <Team />
-          <Faq />
-          <Footer />
+          <ToastContainer position="top-center" autoClose={2000} />
+          <BrowserRouter>
+            <Navbar
+              logout={logout}
+              disconnect={disconnect}
+              setUserMintedAmount={setUserMintedAmount}
+              setMaxMintAmount={setMaxMintAmount}
+              setPrice={setPrice}
+              setImages={setImages}
+              connection={connection}
+              readContract={readContract}
+              getTokens={getTokens}
+              wallet={wallet}
+              price={price}
+              images={images}
+              userMintedAmount={userMintedAmount}
+              maxMintAmount={maxMintAmount}
+            />
+
+            <Routes>
+              <Route
+                exact
+                path="/"
+                element={
+                  <>
+                    <LandingSection />
+                    <Lore />
+                    <NFT />
+                    <Roadmap enable_Modal={enable_Modal} />
+                    <CoreTeam />
+                    <Team />
+                    <Faq />
+                    <Footer />
+                  </>
+                }
+              />
+              <Route
+                exact
+                path="/mint"
+                element={
+                  <>
+                    <Mint
+                      wallet={wallet}
+                      price={price}
+                      images={images}
+                      userMintedAmount={userMintedAmount}
+                      maxMintAmount={maxMintAmount}
+                      disconnect={disconnect}
+                      connection={connection}
+                      readContract={readContract}
+                      getTokens={getTokens}
+                      logout={logout}
+                      setUserMintedAmount={setUserMintedAmount}
+                      setMaxMintAmount={setMaxMintAmount}
+                      setPrice={setPrice}
+                      setImages={setImages}
+                    />
+                    <Footer />
+                  </>
+                }
+              />
+            </Routes>
+          </BrowserRouter>
+
           {showVidModal && (
             <SharedModal closeModal={setShowVidModal} video_src={video_src} />
           )}
--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const discord = require("../../images/navbar/logo_discord.webp");
 const header_desc = require("../../images/navbar/header_des.gif");
@@ -6,41 +7,69 @@ const logo = require("../../images/navbar/logo2desktop.png");
 const opensea = require("../../images/navbar/logo_opensea.svg").default;
 const twitter = require("../../images/navbar/logo_twitter.webp");
 
-const Navbar = () => {
+const Navbar = ({
+  logout,
+  disconnect,
+  setUserMintedAmount,
+  setMaxMintAmount,
+  setPrice,
+  setImages,
+  connection,
+  readContract,
+  getTokens,
+  wallet,
+  price,
+  images,
+  userMintedAmount,
+  maxMintAmount,
+}) => {
+  const navigate = useNavigate();
   return (
     <>
       <div className="flex justify-between  fixed w-full bg-background-img z-50 border-b-4 border-red-400">
-        <div className="w-[140px] m-4">
+        <div onClick={() => navigate("/")} className="w-[140px] m-4">
           <img src={logo} alt="We_Above" />
         </div>
 
         <div className="flex  justify-center">
           <div className="flex justify-end items-center font-heebo text-[#a5b4bd] text-lg">
-            <div className="px-10 ">LORE</div>
-            <div className="px-10 ">NFT</div>
-            <div className="px-10 ">ROADMAP</div>
+            <div className="px-10 hover:text-black">LORE</div>
+            <div className="px-10 hover:text-black">NFT</div>
+            <div className="px-10 hover:text-black">ROADMAP</div>
           </div>
           <div className="flex justify-center relative top-[67%] mt-[-3%]">
             <img src={header_desc} alt="/" className="clip-path-mypolygon" />
           </div>
-          <div className="flex justify-start items-center font-heebo text-[#a5b4bd] text-lg">
-            <div className="px-10">CREW</div>
-            <div className="px-10">FACTIONS</div>
-            <div className="px-10">FAQ</div>
+          <div className="flex justify-start items-center  font-heebo text-[#a5b4bd] text-lg">
+            <div className="px-10 hover:text-black">CREW</div>
+            <div className="px-10 hover:text-black">FACTIONS</div>
+            <div className="px-10 hover:text-black">FAQ</div>
           </div>
         </div>
-
-        <div className="flex justify-evenly">
-          <div className="w-[70px] p-3">
-            <img src={twitter} alt="/" />
-          </div>
-          <div className="w-[70px] p-3">
-            <img src={discord} alt="/" />
-          </div>
-          <div className="w-[70px] p-3">
-            <img src={opensea} alt="/" />
-          </div>
-        </div>
+        <button
+          onClick={async () => {
+            logout
+              ? (async function () {
+                  await disconnect();
+                  await setUserMintedAmount(0);
+                  await setMaxMintAmount("-");
+                  await setPrice("-");
+                  await setImages([]);
+                  navigate("/");
+                })()
+              : (async function () {
+                  await connection();
+                  await readContract();
+                  await getTokens();
+                  navigate("/mint");
+                })();
+          }}
+          className={` hidden lg:inline-block lg:ml-auto lg:mr-3 py-2 px-6 hover:text-xl hover:text-black  text-lg font-heebo text-[#a5b4bd] font-bold  rounded-xl transition duration-200 ${
+            logout ? "hover:before:content-['Disconnect:']" : ""
+          }`}
+        >
+          {wallet}
+        </button>
       </div>
     </>
   );
